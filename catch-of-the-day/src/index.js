b397bc86be25f00ef0e9328a5fe64b9bcc9085b2
Steph Monette <steph@muledesign.com>
2018-09-26 16:16:25 -0700

    starting work on catch of the day

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import Storepicker from './components/storepicker'
+import App from './components/App'
+
+import "./css/style.css"
+
+render(<Storepicker />, document.querySelector('#main'))
